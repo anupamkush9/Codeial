6e6c4fbdca2261b4fcbd111cb9191ef1de84f237
Anuj <anujkushwaha664@gmail.com>
2024-01-05 01:12:06 +0530

    connection established with mongodb using moongoose in cofig folder

---
+++ config/mongoose.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+main().then(()=>{
+    console.log('connection to database established');
+}).catch((err)=>{
+    console.log('could not  connected to database');
+})
+
+async function main(){
+    await mongoose.connect('mongodb://127.0.0.1:27017/Codeial');
+}
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+
+db.once('open', function(){
+    console.log('Connected to Database :: MongoDB');
+});
+
+
+module.exports = db;
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const path = require('path');
 const port = 8000;
+const db = require('./config/mongoose');
 
 app.use(express.urlencoded());
 app.use(cookieParser());
