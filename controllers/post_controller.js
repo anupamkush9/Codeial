fba2d6db979d31f05784a56e82855d9c043b5fe4
Anuj <anujkushwaha664@gmail.com>
2024-01-08 23:24:59 +0530

    layout body height maintained

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -4,6 +4,8 @@ module.exports.create= function(req,res){
     Post.create({
         content : req.body.content,
         user : req.user._id
+    }).then((data)=>{
+        res.redirect('back');
     }).catch((err)=>{
         console.log("con't create post", err);
     })
