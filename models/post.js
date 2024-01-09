ddce8f57fdd16c163039e6a2f54deec1b9553fe1
Anuj <anujkushwaha664@gmail.com>
2024-01-09 22:44:31 +0530

    comment field changed to comments

--- models/post.js
+++ models/post.js
@@ -11,7 +11,7 @@ const postSchema = new mongoose.Schema({
         ref : 'user'
     },
     // include arrays of ids of all comments in this post
-    comment : [
+    comments : [
         {
             type : mongoose.Schema.Types.ObjectId,
             ref : 'comment'
