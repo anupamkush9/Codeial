d0f34188b2fc7e4659649c894f261e903aa2e213
Anuj <anujkushwaha664@gmail.com>
2024-01-11 09:47:41 +0530

    destroy post controller added

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create= function(req,res){
     Post.create({
@@ -8,5 +9,29 @@ module.exports.create= function(req,res){
         res.redirect('back');
     }).catch((err)=>{
         console.log("con't create post", err);
+    })
+}
+
+module.exports.destroy = function(req,res){
+    console.log(req.params.id);
+    Post.findById(req.params.id).then((post)=>{
+        // checking user who created post is deleting or not
+        // .id means converting object id into string.
+        if(post.user == req.user.id){
+            //Deleting post
+            Post.deleteOne({_id : req.params.id}).catch((err)=>{
+                console.log(err);
+            })
+            //Deleting post comments
+            Comment.deleteMany({post : req.params.id}).catch((err)=>{
+                console.log("err while deleting comment");
+            })
+
+            res.redirect('/');
+        }else{
+            res.redirect('/');
+        }
+    }).catch((err)=>{
+        console.log("could not find post with given id in destroy function of post",err);
     })
 }
