46ee1885093436641c88af6d6a632f141476923d
Anuj <anujkushwaha664@gmail.com>
2024-01-14 10:15:59 +0530

    In created and destroy contoller if req is xhr then res is created

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -6,6 +6,14 @@ module.exports.create= function(req,res){
         content : req.body.content,
         user : req.user._id
     }).then((data)=>{
+        if(req.xhr){
+            return res.status(200).json({
+                data : {
+                    post : data,
+                    message : 'post created successfully !'
+                }
+            })
+        }
         req.flash('success', 'Post created successfully !');
         res.redirect('back');
     }).catch((err)=>{
@@ -14,7 +22,7 @@ module.exports.create= function(req,res){
 }
 
 module.exports.destroy = function(req,res){
-    console.log(req.params.id);
+    // console.log(req.params.id);
     Post.findById(req.params.id).then((post)=>{
         // checking user who created post is deleting or not
         // .id means converting object id into string.
@@ -27,6 +35,17 @@ module.exports.destroy = function(req,res){
             Comment.deleteMany({post : req.params.id}).catch((err)=>{
                 console.log("err while deleting comment");
             })
+
+            if(req.xhr){
+                console.log("xhr");
+                return res.status(200).json({
+                    data : {
+                        post_id : req.params.id,
+                        message : 'post  deleted successfully !'
+                    }
+                })
+            }
+            console.log("not  xhr");
             req.flash('success', 'Post deleted successfully !');
             res.redirect('/');
         }else{
