364832e129a221b6ee9626ecb59da907c81e4a7e
Anuj <anujkushwaha664@gmail.com>
2024-01-16 20:38:02 +0530

    profile image saving setting did in update action or controller.

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const fs = require('fs'); // fs -> file system
+const path = require('path');
 
 module.exports.userProfile = function(req,res){
     User.findById(req.params.id).then((user)=>{
@@ -12,9 +14,40 @@ module.exports.userProfile = function(req,res){
 
 module.exports.update = function(req,res){
     if(req.user.id == req.params.id){
-        User.findByIdAndUpdate(req.params.id, req.body).then((user)=>{
-           return res.redirect('back');
-        });
+        // User.findByIdAndUpdate(req.params.id, req.body).then((user)=>{
+        //    return res.redirect('back');
+        // });
+
+        // console.log(req.body); // body not accessible because form enctype is multipart
+       User.findById(req.params.id).then((user)=>{
+ 
+            User.uploadedAvatar(req,res,function(err){
+                    if(err){console.log('*****Multer ERROR : ',err)};
+
+                    user.name = req.body.name;
+                    user.email = req.body.email;
+
+                    if(req.file){
+
+                        // existsSysnc -> checks deleting file exists or not
+                        if(user.avatar && fs.existsSync(path.join(__dirname,'..', user.avatar))){
+                            // deleting already existing avatar image
+                            fs.unlinkSync(path.join(__dirname,'..', user.avatar));
+                        }
+
+                        //this is saving the path of uploaded file into the avatar field in the user.
+                        user.avatar = User.avatarPath + "/" + req.file.filename;
+                    }
+
+                    user.save().then((data)=>{
+                        console.log("user updated successfully");
+                    }).catch((err)=>{
+                        console.log("err in saving", err);
+                    })
+                    return res.redirect('back');
+            })
+              
+        })
     }else{
         return res.status(401).send('Unauthorized access');
     }
