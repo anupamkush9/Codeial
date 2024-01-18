3d5956285b3cb9de621a01b30102750c955dae21
Anuj <anujkushwaha664@gmail.com>
2024-01-18 17:00:27 +0530

    routes for google authentication added

--- routes/user.js
+++ routes/user.js
@@ -18,4 +18,7 @@ router.post('/create-session', passport.authenticate(
 
 router.get('/sign-out', userContoller.destroySession);
 
+router.get('/auth/google', passport.authenticate('google', {scope : ['profile','email']}));
+router.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/user/sign-in'}), userContoller.createSession);
+
 module.exports = router;
